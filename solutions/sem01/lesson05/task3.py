5cfa34b2ff8891405a7371dda0fd0f429072e851
Vika <kostrykova.v.a.12@gmail.com>
2026-03-11 00:04:55 +0300

    123

--- solutions/sem01/lesson05/task3.py
+++ solutions/sem01/lesson05/task3.py
@@ -1,7 +1,8 @@
 def is_punctuation(text: str) -> bool:
     if len(text) == 0:
         return False
-    punctuation_marks = "!#$%&'()*+,-./:;<=>?@[\]^_{|}~`\""
+    punctuation_marks =  "!#$%&'()*+,-./:;<=>?@[\\]^_{|}~`\""
+       
     for char in text:
         if char not in punctuation_marks:
             return False
