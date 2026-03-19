7704f3159d2e5e46a46fcc5b321242d35012da8b
Vika <kostrykova.v.a.12@gmail.com>
2026-03-19 14:34:09 +0300

    HW les 5

--- solutions/sem02/lesson05/task1.py
+++ solutions/sem02/lesson05/task1.py
@@ -9,4 +9,11 @@ def can_satisfy_demand(
     costs: np.ndarray,
     resource_amounts: np.ndarray,
     demand_expected: np.ndarray,
-) -> bool: ...
+) -> bool:
+    M, N = costs.shape
+    if M != len(resource_amounts) or N != len(demand_expected):
+        raise ShapeMismatchError
+
+    resurce_need = costs @ demand_expected
+    result = np.all(resurce_need <= resource_amounts)
+    return result
--- solutions/sem02/lesson05/task2.py
+++ solutions/sem02/lesson05/task2.py
@@ -8,4 +8,26 @@ class ShapeMismatchError(Exception):
 def get_projections_components(
     matrix: np.ndarray,
     vector: np.ndarray,
-) -> tuple[np.ndarray | None, np.ndarray | None]: ...
+) -> tuple[np.ndarray | None, np.ndarray | None]: 
+    N1, N2 = matrix.shape
+    if N1 != N2 or N2 != len(vector):
+        raise ShapeMismatchError
+    
+    if  np.isclose(np.linalg.det(matrix) , 0):
+        return (None, None)
+    
+    # proj = []
+    # ortog_comp = []
+
+    # for e in matrix:
+    #     projectoin = ((vector@e)/(e@e)*e)
+    #     orth = vector - projectoin
+    #     proj.append(projectoin)
+    #     ortog_comp.append(orth)
+    # return (np.array(projectoin), np.array(ortog_comp))
+
+    coordinates  = (matrix @ vector) / np.sum(matrix*matrix, axis = 1)
+    projections = coordinates [:, np.newaxis]*matrix
+    orthogonals  = vector - projections
+
+    return (projections, orthogonals )
